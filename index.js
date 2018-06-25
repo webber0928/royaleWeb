c9448e49347208a17a8b111ac24385e956ce4525
webber.wu <bird23074035@gmail.com>
2018-06-25 12:00:17 +0800

    把id拉出來

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ async function getData() {
     const battles = $('[data-battle-type=friendly]');
 
     battles.each(function(i, elem) {
-      a = $(this).find('.blue.label.ribbon');
-      b = $(this).find('.red.label.ribbon');
-      play1 = $(this).find('[href="/player/PG829G02/battles"]').text();
-      play2 = $(this).find('[href="/player/8C8Q80QRG/battles"]').text();
-      date = $(this).find('.ui.two.column.mobile.reversed.stackable.grid').text();
-      date = date.split('ago')[0];
-      timestamp = $(this).attr('id').replace('battle_', '') *1000;
+      let a = $(this).find('.blue.label.ribbon');
+      let b = $(this).find('.red.label.ribbon');
+      let play1 = $(this).find('[href="/player/PG829G02/battles"]').text();
+      let play2 = $(this).find('[href="/player/8C8Q80QRG/battles"]').text();
+      let id = $(this).attr('id');
+      let timestamp = id.replace('battle_', '') *1000;
       date = moment.tz(timestamp, "Asia/Taipei");
 
       console.log(play1);
